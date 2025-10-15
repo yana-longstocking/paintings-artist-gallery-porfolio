ebe165c772b5bbd2dc78367a7d4f8337e46dceaf
Yana Chernenko <yanamyrna@gmail.com>
2025-10-15 22:35:34 +0200

    adjust paths and routing

--- astro-gallery/astro.config.mjs
+++ astro-gallery/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yana-longstocking.github.io/',
-  base: '/paintings-artist-gallery-porfolio'
+  site: 'https://yana-longstocking.github.io',
+  base: '/paintings-artist-gallery-porfolio/'
 });
