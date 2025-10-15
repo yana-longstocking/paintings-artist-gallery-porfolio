595a07952c92b84724aa25345b4ea9482af6791a
Yana Chernenko <yanamyrna@gmail.com>
2025-10-15 22:22:29 +0200

    updated github deploy config

--- astro-gallery/astro.config.mjs
+++ astro-gallery/astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://yana-longstocking.github.io/',
+  base: '/paintings-artist-gallery-porfolio'
+});
