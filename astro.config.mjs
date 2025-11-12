aa3c8e0128d6a5bddf10b19f1cdead09566a6abc
Yana Chernenko <yanamyrna@gmail.com>
2025-11-12 22:43:55 +0100

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://yana-longstocking.github.io",
-  base: "/paintings-artist-gallery-porfolio/",
+  site: "https://irynaromanska.com",
+  base: "/",
 });
